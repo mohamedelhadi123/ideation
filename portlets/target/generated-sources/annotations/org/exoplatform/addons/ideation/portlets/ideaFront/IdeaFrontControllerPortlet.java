aeafb5c90463e65108d4c14ac9bf8a916e929c93
Ali Hamdi <ahamdi@exoplatform.com>
2019-04-03 13:18:45 +0100

    Cleanup target folders and node_modules folder

--- portlets/target/generated-sources/annotations/org/exoplatform/addons/ideation/portlets/ideaFront/IdeaFrontControllerPortlet.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.addons.ideation.portlets.ideaFront;
-import javax.annotation.Generated;
-@Generated(value={})
-public class IdeaFrontControllerPortlet extends juzu.bridge.portlet.JuzuPortlet {
-@Override
-protected String getApplicationName(javax.portlet.PortletConfig config) {
-return "org.exoplatform.addons.ideation.portlets.ideaFront";
-}
-}
--- portlets/target/generated-sources/annotations/org/exoplatform/addons/ideation/portlets/ideationAdministration/IdeationAdministrationControllerPortlet.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.addons.ideation.portlets.ideationAdministration;
-import javax.annotation.Generated;
-@Generated(value={})
-public class IdeationAdministrationControllerPortlet extends juzu.bridge.portlet.JuzuPortlet {
-@Override
-protected String getApplicationName(javax.portlet.PortletConfig config) {
-return "org.exoplatform.addons.ideation.portlets.ideationAdministration";
-}
-}
